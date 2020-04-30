b1e93a4356c3f643ae2335dde5879ff01fa2f835
mesung <myhs2002@gmail.com>
2020-04-30 18:42:25 +0900

    프로세스 구

---
+++ osExam/src/main/java/com/mesung/interrupt/SyncMemory.java
@@ -0,0 +1,14 @@
+package com.mesung.interrupt;
+
+public class SyncMemory {
+    public static void main(String [] args) {
+        syncMemory();
+    }
+
+    public static int syncMemory() {
+        System.out.println("hello");
+        Integer data;
+        data = new Integer(1);
+        return 0;
+    }
+}
